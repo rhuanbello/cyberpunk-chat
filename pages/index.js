73647e6c4fd5cffde1d47c6420e2046d23e93588
rhuanbello <rhuambello@gmail.com>
2022-01-25 20:07:09 -0300

    feat: Aula 02 && Validation Challenge

--- pages/index.js
+++ pages/index.js
@@ -5,37 +5,9 @@ import {
   TextField,
   Image
 } from '@skynexui/components'
+import { useEffect, useState } from 'react';
 import appConfig from '../config.json';
-
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      list-style: none;
-    }
-    body {
-      font-family: 'Open Sans', sans-serif;
-    }
-    /* App fit Height */ 
-    html, body, #__next {
-      min-height: 100vh;
-      display: flex;
-      flex: 1;
-    }
-    #__next {
-      flex: 1;
-    }
-    #__next > * {
-      flex: 1;
-    }
-    /* ./App fit Height */ 
-  `}</style>
-  );
-}
+import { useRouter } from 'next/router';
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -57,21 +29,30 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'rhuanbello';
+  const [username, setUsername] = useState('');
+  const [warning, setWarning] = useState(false);
+  const route = useRouter();
+
+  const handleEnterChat = () => {
+    if (username.length > 2) {
+      route.push('/chat')
+    } else {
+      setWarning(true)
+    }
+  }
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          backgroundImage: 'url(https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)',
+          backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
         }}
       >
         <Box
           styleSheet={{
+            position: 'relative', 
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-between',
@@ -82,24 +63,37 @@ export default function PaginaInicial() {
             width: '100%', maxWidth: '700px',
             borderRadius: '5px', padding: '32px', margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            background: 'rgba( 255, 255, 255, 0.3 )',
+            boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+            backdropFilter: 'blur( 3px )',
+            borderRadius: '10px',
+            border: '1px solid rgba( 255, 255, 255, 0.18 )',
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(e) => {
+              e.preventDefault()
+              handleEnterChat()
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
+            <Titulo tag="h2">Welcome to Night City</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
 
             <TextField
               fullWidth
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value)
+                setWarning(false)
+              }}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
@@ -111,18 +105,29 @@ export default function PaginaInicial() {
             />
             <Button
               type='submit'
-              label='Entrar'
+              label='Login'
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
+                mainColor: appConfig.theme.colors.neutrals['300'],
                 mainColorLight: appConfig.theme.colors.primary[400],
                 mainColorStrong: appConfig.theme.colors.primary[600],
               }}
             />
+            {warning && (
+              <Box as="p"
+                styleSheet={{
+                  position: 'absolute',
+                  bottom: '18%',
+                  left: '5%',
+                  fontSize: '14px',
+                  color: appConfig.theme.colors.neutrals["000"],
+                }}>
+                {(username.length > 0 && username.length <= 2)  ? 'Quantidade de caracteres inválida.' : 'Informe um nome de usuário.'}
+              </Box>
+            )}
           </Box>
           {/* Formulário */}
-
 
           {/* Photo Area */}
           <Box
@@ -140,24 +145,30 @@ export default function PaginaInicial() {
               minHeight: '240px',
             }}
           >
-            <Image
-              styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
-              }}
-              src={`https://github.com/${username}.png`}
-            />
-            <Text
-              variant="body4"
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: '3px 10px',
-                borderRadius: '1000px'
-              }}
-            >
-              {username}
-            </Text>
+            {username.length > 2 && (
+              <>
+                <Image
+                  styleSheet={{
+                    borderRadius: '50%',
+                    marginBottom: '16px',
+                    display: username.length > 2 ? 'block' : 'none',
+                  }}
+                  src={`https://github.com/${username}.png`}
+                />
+                <Text
+                  variant="body4"
+                  styleSheet={{
+                    color: appConfig.theme.colors.neutrals[200],
+                    backgroundColor: appConfig.theme.colors.neutrals[900],
+                    padding: '3px 10px',
+                    borderRadius: '1000px',
+                    display: username.length > 2 ? 'block' : 'none',
+                  }}
+                >
+                  {username}
+                </Text>
+              </>
+            )}
           </Box>
           {/* Photo Area */}
         </Box>
